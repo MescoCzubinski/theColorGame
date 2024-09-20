beb9023ee0b00d7539a5eb7c7c081b91d9971f48
Dradones <mieszkoczubinski@gmail.com>
2024-09-20 20:45:12 +0200

    input color display and input style

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function main(){
-    document.body.style.backgroundColor = radnomColor();
+    const color = radnomColor();
+    document.body.style.backgroundColor = "#" + color;
+
+    color = parseFloat(color) - 100000;
+    document.getElementById("hash").style.color = "#" + color;
 }
 window.addEventListener('click', main)
 window.addEventListener('load', main)
@@ -7,15 +11,20 @@ window.addEventListener('load', main)
 function radnomColor(){
     let color = ""
     for(i=0; i<3; i++){
-        var number = Math.random() * (255-0) + 1
+        var number = Math.random() * (235-20) + 1
         number = (Math.round(number)).toString(16);
         color += number;
     }
-    return "#" + color;
+    return color;
 }
 
-window.addEventListener('click', function() {
-    let insertColorElement = document.getElementById("insertColor").value;
-    
-    document.getElementById("insertColor").value = "#" + insertColorElement;
-});
+window.addEventListener('click', inputing)
+function inputing(){
+    const insertColor1Element = document.getElementById("insertColor1");
+    const insertColor2Element = document.getElementById("insertColor2");
+    const insertColor3Element = document.getElementById("insertColor3");
+
+    for(i=0; i<3; i++){
+
+    }
+}
