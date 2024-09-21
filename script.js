994c0e46d9ef439fe240bca4d830a0a769ee8580
Dradones <mieszkoczubinski@gmail.com>
2024-09-21 22:47:12 +0200

    fullly work music made by Jan Baczkowski

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ function main(){
     document.body.style.backgroundColor = color;
     document.getElementById("hash").style.color = color;
     document.getElementById("testColor").style.backgroundColor = opositeColor;
+    document.getElementById("testColor").style.borderColor = opositeColor;
     document.getElementById("upperText").style.color = opositeColor;
     document.getElementById("bottomText").style.color = opositeColor;
     document.getElementById("leftText").style.color = opositeColor;
 
     for(i=1; i<4; i++){
         document.getElementById("insertColor"+i).style.backgroundColor = color;
+        document.getElementById("insertColor"+i).style.borderColor = opositeColor;
+
     }
 }
 window.addEventListener('load', main);
@@ -43,14 +46,20 @@ function inputing(){
     for(let i = 0; i < 3; i++){
         let input = insertColorElements[i].value.replace(/[^0-9a-f]/gi, "");
         color += input;
+        if(input.length === 2){
+            insertColorElements[i+1].value.blur();
+        }
     }
-    document.getElementById("testColor").style.backgroundColor = color;
-    document.getElementById("testColor").style.color = color;
-    document.getElementById("upperText").style.color = color;
-    document.getElementById("bottomText").style.color = color;
-    document.getElementById("leftText").style.color = color;
 
-    for(i=1; i<4; i++){
-        document.getElementById("insertColor"+i).style.color = color;
+    if(color.length === 7){
+        document.getElementById("testColor").style.backgroundColor = color;
+        document.getElementById("hash").style.color = document.getElementById("testColor").style.borderColor;
     }
+    
 }
+
+window.addEventListener('click', event => {
+    const audio = document.querySelector("audio");
+    audio.play();
+    audio.loop = true;
+});
